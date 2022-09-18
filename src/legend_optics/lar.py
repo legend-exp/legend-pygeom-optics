3116f0d070ce66330a4337b204b786e9bffcd8a9
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-09-18 14:45:19 +0200

    A couple of fixes, not code really works

--- src/legend_optics/lar.py
+++ src/legend_optics/lar.py
@@ -9,24 +9,24 @@ u = pint.get_application_registry()
 
 
 def lar_dielectric_constant_bideau_mehu(λ: Quantity) -> float:
-    """Calculate the dielectric constant of LAr from the Bideau-Sellmeier formula [Bideau-Mehu1980]_.
+    """Calculate the dielectric constant of LAr for a given photon wavelength.
+
+    From the Bideau-Sellmeier formula [Bideau-Mehu1980]_.
 
     .. [Bideau-Mehu1980] https://doi.org/10.1016/0022-4073(81)90057-1
     """
-    if not λ.check("[energy]"):
-        raise ValueError("input does not look like an energy value")
+    if not λ.check("[length]"):
+        raise ValueError("input does not look like a wavelength")
 
-    # for later calculations
-    λ.ito(u.um)
-
+    # wavelength must be > 110.0 nm
     if λ < 110 * u.nm:
-        return 1.0e4  # lambda MUST be > 110.0 nm
+        return 1.0e4
 
     # equation for n-1
     ϵ = 1.2055e-2 * (
-        0.2075 / (91.012 - 1 / λ**2)
-        + 0.0415 / (87.892 - 1 / λ**2)
-        + 4.3330 / (214.02 - 1 / λ**2)
+        0.2075 * λ**2 / (91.012 * λ**2 - 1 * u.um**2)
+        + 0.0415 * λ**2 / (87.892 * λ**2 - 1 * u.um**2)
+        + 4.3330 * λ**2 / (214.02 * λ**2 - 1 * u.um**2)
     )
     ϵ *= 2 / 3  # Bideau-Sellmeier -> Clausius-Mossotti
     ϵ *= 1.396 / 1.66e-3  # density correction (Ar gas -> LAr liquid)
@@ -38,26 +38,25 @@ def lar_dielectric_constant_bideau_mehu(λ: Quantity) -> float:
 
 
 def lar_dielectric_constant_cern2020(λ: Quantity) -> float:
-    """Calculate the dielectric constant of LAr [Babicz2020]_.
+    """Calculate the dielectric constant of LAr for a given photon wavelength.
+
+    From [Babicz2020]_.
 
     .. [Babicz2020] https://doi.org/10.1088/1748-0221/15/09/P09009
     """
-    if not λ.check("[energy]"):
-        raise ValueError("input does not look like an energy value")
-
-    # for later calculations
-    λ.ito(u.nm)
+    if not λ.check("[length]"):
+        raise ValueError("input does not look like a wavelength")
 
     x = 0.334 + (
-        (0.100 * λ**2) / (λ**2 - 106.6**2)
-        + (0.008 * λ**2) / (λ**2 - 908.3**2)
+        (0.100 * λ**2) / (λ**2 - (106.6 * u.nm) ** 2)
+        + (0.008 * λ**2) / (λ**2 - (908.3 * u.nm) ** 2)
     )
 
     return (3 + 2 * x) / (3 - x)  # solve Clausius-Mossotti
 
 
 def lar_dielectric_constant(λ: Quantity, method: str = "cern2020") -> float:
-    """Calculate the dielectric constant of LAr.
+    """Calculate the dielectric constant of LAr for a given photon wavelength.
 
     See Also
     --------
